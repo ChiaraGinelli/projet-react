fe969456b4ddd015c7034a3709f55bfd831fcdc6
Chiara <ginelli.ch@gmail.com>
2026-02-10 10:49:58 +0100

    component

---
+++ mon-projet/src/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+  return (
+  <div>
+    <h1>Mon portfolio</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A earum voluptatem, veniam eaque fuga id, nemo, nostrum in rem eveniet pariatur placeat dignissimos temporibus recusandae? Quidem sit esse illum ex?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A earum voluptatem, veniam eaque fuga id, nemo, nostrum in rem eveniet pariatur placeat dignissimos temporibus recusandae? Quidem sit esse illum ex?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A earum voluptatem, veniam eaque fuga id, nemo, nostrum in rem eveniet pariatur placeat dignissimos temporibus recusandae? Quidem sit esse illum ex?</p>
+    <img src="https://tse4.mm.bing.net/th/id/OIP.qMsDRRAqWgIHQic9_dWvdQHaE2?rs=1&pid=ImgDetMain&o=7&rm=3" alt="photographie" />
+    <br />
+    <button>Me contacter</button>
+  </div>)
+}
+export default Card
